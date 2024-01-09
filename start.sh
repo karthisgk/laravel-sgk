0e07bd0596071b9fd89a4927bab63d2041378af8
karthick-zoftsolutions <karthick@zoftsolutions.com>
2024-01-09 15:48:03 +0530

    added start script

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
-cp /home/site/wwwroot/vite.config.js /home/site/
+cp /home/site/default /etc/nginx/sites-available/default
+service nginx reload
+
+file="/home/site/wwwroot/.env"
+
+if [ ! -e "$file" ]; then
+    cd /home/site/wwwroot && cp -n .env.example .env 
+    php artisan key:generate
+fi
